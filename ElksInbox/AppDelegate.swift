b0fbf5416d091238ed0c4757088dd45c3a966119
Johannes Lundberg <johannes.lundberg@nospam-gmail.com>
2014-07-10 15:54:04 +0200

    Initial Commit

---
+++ ElksInbox/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ElksInbox
+//
+//  Created by Johannes Lundberg on 2014-07-10.
+//
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+  @IBOutlet var window: NSWindow
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification?) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification?) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ ElksInbox/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  ElksInbox
+//
+//  Created by Johannes Lundberg on 2014-07-10.
+//
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
