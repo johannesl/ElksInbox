b03ec80ffbee23bba09e0ae52d807a2c34be4ff8
Johannes Lundberg <johannes.lundberg@nospam-gmail.com>
2014-09-15 13:35:08 +0200

    Rough base layout.. Warming up!

--- ElksInbox/AppDelegate.swift
+++ ElksInbox/AppDelegate.swift
@@ -8,13 +8,32 @@
 
 import Cocoa
 
+class TestSource : NSObject, NSOutlineViewDataSource {
+  
+  func numberOfChildrenOfItem(item : Any) {
+  }
+
+  func isItemExandable (item : Any) {
+  }
+  
+  func child:ofItem (item : Any) -> Bool {
+  }
+  func objeectValueForTableColumn:byItem -> Bool (item: Any) {
+  
+  }
+}
+
 class AppDelegate: NSObject, NSApplicationDelegate {
                             
   @IBOutlet var window: NSWindow
-
-
+  @IBOutlet var sourceList: NSOutlineView
+  
   func applicationDidFinishLaunching(aNotification: NSNotification?) {
     // Insert code here to initialize your application
+    
+    var numbers : TestSource = TestSource()
+    
+    sourceList.setDataSource(numbers)
   }
 
   func applicationWillTerminate(aNotification: NSNotification?) {
